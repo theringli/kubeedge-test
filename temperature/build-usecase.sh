40b5baf34c873a9d61c0d272d9c2092a8c198d5b
Jerome Ring <jerome.ring@computacenter.com>
2021-08-18 10:09:58 +0200

    Added quick shell script

---
+++ temperature/build-usecase.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+case $0 in
+    build)
+        kubectl apply -f timescale-db.yml
+        POD_NAME=$(kubectl get po -l app=timescale -o jsonpath='{.items[0].metadata.name}')
+        kubectl exec -ti ${POD_NAME} -- psql -h 127.0.0.1 -p 5432 -U postgres -c "CREATE ROLE kubeedge WITH LOGIN PASSWORD 'kubeedge'; CREATE ROLE grafana WITH LOGIN PASSWORD 'grafana';"
+        kubectl exec -ti ${POD_NAME} -- psql -U postgres -c "CREATE DATABASE grafana WITH OWNER grafana"
+        kubectl exec -ti ${POD_NAME} -- psql -U postgres -c "CREATE DATABASE demo WITH OWNER kubeedge"
+        kubectl apply -f grafana.yml
+        kubectl apply -f deviceModel.yaml
+        kubectl apply -f deviceInstance.yaml
+        kubectl apply -f kubeedge-database.yml
+        kubectl apply -f cpu-temp-sensor.yaml
+        ;;
+    
+    delete)
+        kubectl delete -f timescale-db.yml
+        kubectl delete -f grafana.yml
+        kubectl delete -f deviceModel.yaml
+        kubectl delete -f deviceInstance.yaml
+        kubectl delete -f kubeedge-database.yml
+        kubectl delete -f cpu-temp-sensor.yaml
+        ;;
+
+    rebuild)
+        ./build-usecase.sh delete
+        ./build-usecase.sh build
+        ;;
+
+    *)
+        echo "Use arguments: build, delete, or rebuild"
+        exit(0)
+        ;;
+esac
